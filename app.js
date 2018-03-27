8e6ffbdd13faa965b7292a49bdce789a0baf2ec5
cascalovesguts <37838452+cascalovesguts@users.noreply.github.com>
2018-03-27 09:22:23 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,65 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json');
+
+client.on('ready',() => {
+	console.log('I\'m Online\nI\'m Online');
+});
+
+var prefix = "~"
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'tag1')) {
+		message.channel.sendMessage('Warning! Proceed with caution, because the following message contains mentions of violence or violent imagery!');
+	}
+});
+
+var prefix = "~"
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'tag2')) {
+		message.channel.sendMessage('Warning! Proceed with caution, because the following message contains mentions of sexual violence!');
+	}
+});
+
+var prefix = "~"
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'tag3')) {
+		message.channel.sendMessage('Warning! Proceed with caution, because the following message contains mentions of pedophilia!');
+	}
+});
+
+var prefix = "~"
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'tag2')) {
+		message.channel.sendMessage('Warning! Proceed with caution, because the following message contains mentions of self harm!');
+	}
+});
+
+var prefix = "~"
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'reqtag')) {
+		message.channel.sendMessage('Warning! Proceed with caution, because the following message contains mentions or content involving one of the requested trigger tags!');
+	}
+});
+
+var prefix = "~"
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'in the name of the moon')) {
+		message.channel.sendMessage('i will destroy you!');
+	}
+});
+
+var prefix = "~"
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'help me usagi!')) {
+		message.channel.sendMessage('i am on my way!');
+	}
+});
+
+client.login(process.env.BOT_TOKEN);
